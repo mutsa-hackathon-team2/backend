78ee5a4a645e99e2068eee423ec93ee94a8f66d6
박수민 <soomini030113@gmail.com>
2026-07-23 16:48:42 +0900

    [Feat] HTTPS 적용에 따른 CORS/세션 쿠키 설정 변경

--- src/main/java/cultureland/backend/global/config/WebConfig.java
+++ src/main/java/cultureland/backend/global/config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://frontend-yw8e.vercel.app"
+                )
                 .allowedMethods(
                         "GET",
                         "POST",
