c38e461e7d87eac7c162245579fb841edebeb36d
uaeoon <hhaaee9153@g.hongik.ac.kr>
2026-07-23 14:32:03 +0900

    [Feat] 카카오 로그인 프론트 리다이렉트 구현

--- src/main/java/cultureland/backend/auth/controller/AuthController.java
+++ src/main/java/cultureland/backend/auth/controller/AuthController.java
@@ -2,25 +2,42 @@ package cultureland.backend.auth.controller;
 
 import cultureland.backend.auth.dto.LoginResponse;
 import cultureland.backend.auth.service.AuthService;
-import cultureland.backend.global.apiPayload.ApiResponse;
+import cultureland.backend.global.session.SessionConst;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/auth/kakao")
+@Controller
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
 
-    @GetMapping("/callback")
-    public ApiResponse<LoginResponse> kakaoCallback(
-            @RequestParam String code
+    @Value("${frontend.redirect-uri}")
+    private String frontendRedirectUri;
+
+    @GetMapping("/kakao/callback")
+    public String kakaoCallback(
+            @RequestParam String code,
+            HttpServletRequest request
     ) {
-        return ApiResponse.onSuccess(
-                authService.loginWithKakao(code)
+        LoginResponse response =
+                authService.loginWithKakao(code);
+
+        HttpSession session = request.getSession();
+
+        request.changeSessionId();
+
+        session.setAttribute(
+                SessionConst.LOGIN_MEMBER_ID,
+                response.getMemberId()
         );
+
+        return "redirect:" + frontendRedirectUri;
     }
 }
