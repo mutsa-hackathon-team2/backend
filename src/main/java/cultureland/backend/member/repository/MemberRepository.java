0771a787aa699018a96b9aa011670a28d8c0b170
uaeoon <hhaaee9153@g.hongik.ac.kr>
2026-07-23 11:41:12 +0900

    [Feat] Member 리포지토리 구현

---
+++ src/main/java/cultureland/backend/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package cultureland.backend.member.repository;
+
+import cultureland.backend.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByKakaoId(Long kakaoId);
+}
